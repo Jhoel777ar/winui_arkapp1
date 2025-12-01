72b101e6f5a8b4ce881d5b86ed5e99fd74c7fabf
ajoeark <ajoeark@gmail.com>
2025-12-01 23:02:12 +0000

    pueba

--- ark_app1/MainWindow.xaml.cs
+++ ark_app1/MainWindow.xaml.cs
@@ -1,31 +1,94 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+using System.Data.SqlClient;
 
 namespace ark_app1
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            AppWindow.SetIcon("Assets/Tiles/GalleryIcon.ico");
+            AppWindow.TitleBar.PreferredTheme = TitleBarTheme.UseDefaultAppMode;
+
+            // Center the window on the screen.
+            CenterWindow();
+        }
+
+        private void CenterWindow()
+        {
+            var area = DisplayArea.GetFromWindowId(AppWindow.Id, DisplayAreaFallback.Nearest)?.WorkArea;
+            if (area == null) return;
+            AppWindow.Move(new PointInt32((area.Value.Width - AppWindow.Size.Width) / 2, (area.Value.Height - AppWindow.Size.Height) / 2));
+        }
+
+        private void AuthenticationComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SqlAuthenticationStackPanel != null)
+            {
+                var selectedItem = (ComboBoxItem)AuthenticationComboBox.SelectedItem;
+                if (selectedItem.Content.ToString() == "SQL Server Authentication")
+                {
+                    SqlAuthenticationStackPanel.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    SqlAuthenticationStackPanel.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        private async void TestConnectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            string serverName = ServerNameTextBox.Text;
+            string connectionString;
+
+            var selectedItem = (ComboBoxItem)AuthenticationComboBox.SelectedItem;
+            if (selectedItem.Content.ToString() == "Windows Authentication")
+            {
+                connectionString = $"Server={serverName};Database=master;Integrated Security=True;";
+            }
+            else
+            {
+                string username = UsernameTextBox.Text;
+                string password = PasswordBox.Password;
+                connectionString = $"Server={serverName};Database=master;User ID={username};Password={password};";
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    ContentDialog dialog = new ContentDialog
+                    {
+                        Title = "Conexión exitosa",
+                        Content = "La conexión al servidor SQL se ha establecido correctamente.",
+                        CloseButtonText = "Aceptar",
+                        XamlRoot = this.Content.XamlRoot
+                    };
+                    await dialog.ShowAsync();
+                }
+                catch (Exception ex)
+                {
+                    ContentDialog dialog = new ContentDialog
+                    {
+                        Title = "Error de conexión",
+                        Content = $"No se pudo establecer la conexión con el servidor SQL.\nError: {ex.Message}",
+                        CloseButtonText = "Aceptar",
+                        XamlRoot = this.Content.XamlRoot
+                    };
+                    await dialog.ShowAsync();
+                }
+            }
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Handle connection logic here
         }
     }
 }
